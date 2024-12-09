8fd350f0fee1330b84e709e657c3e78059933465
Filip Komárek <fkomarek@noreply.git.envs.net>
2024-12-09 14:38:12 +0000

    DELL: windows-3.1-simulator

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ async function displayProjects() {
         { name: 'network-info', owner: 'filip2cz', repo: 'network-info' },
         { name: 'space-image', owner: 'filip2cz', repo: 'space-image' },
         { name: 'prague.fkomarek.eu', owner: 'filip2cz', repo: 'prague.fkomarek.eu' },
-        { name: 'windows-3.1-simulator', owner: 'filip2cz', repo: 'windows-3.1-simulator' },
         { name: 'dns-minimal', owner: 'filip2cz', repo: 'dns-minimal' },
         { name: 'vmware-powercli-running-vms-table', owner: 'filip2cz', repo: 'vmware-powercli-running-vms-table' },
         { name: 'wifi-checker', owner: 'filip2cz', repo: 'wifi-checker' },
